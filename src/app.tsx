59c16d3924b9cc549ffaa455495ded9d768c8950
silviolopesdias <silvio.sandra@live.com>
2024-06-05 05:15:24 -0300

    Iniciando projeto

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import GlobalStyles from './styles/global';
+
+export function App() {
+  return (
+    <>
+      <h1>DevBills</h1>
+
+      <GlobalStyles />
+    </>
+  );
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary: '#00ED64',
+    primaryDark: '#00684A',
+    success: '#00ED64',
+    info: '#016BF8',
+    warning: '#FFC010',
+    error: '#DB3030',
+    white: '#F9FBFA',
+    ligth: '#E8EDEB',
+    neutral: '#C1C7C6',
+    dark: '#001E28',
+    black: '#04141C',
+  },
+};
