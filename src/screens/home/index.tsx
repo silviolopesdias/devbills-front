2d2f251c4c061997bb71307014661cdc58f021e2
silviolopesdias <silvio.sandra@live.com>
2024-06-09 00:15:22 -0300

    Inicio do Main, criação doos componentes Title, Input com Inputmask.

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,13 @@
+import { InputMask } from '@react-input/mask';
 import { Button } from '../../components/button';
+import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
-import { Header } from './styles';
+import { Title } from '../../components/title';
+import { Filters, Header, InputGroup, Main, Section } from './styles';
 
 export function Home() {
   return (
+    <>
     <Header>
       <Logo />
       <div>
@@ -11,5 +15,34 @@ export function Home() {
         <Button>Nova Transação</Button>
       </div>
     </Header>
+    <Main>
+      <Section>
+        <Filters>
+          <Title title="Saldo" subtitle="Receitas e despesas no período"/>
+          <InputGroup>
+
+            <InputMask 
+            component={Input}
+            mask='dd/mm/yyyy'
+            replacement={{d:/\d/, m:/\d/, y:/\d/}}
+            variant='dark'
+            label="Inicio" 
+            placeholder="dd/mm/yyyy"
+            />
+
+            <InputMask 
+            component={Input}
+            mask='dd/mm/yyyy'
+            replacement={{d:/\d/, m:/\d/, y:/\d/}}
+            variant='dark'
+            label="Fim"
+            placeholder="dd/mm/yyyy"
+            />   
+
+          </InputGroup>
+        </Filters>
+      </Section>
+    </Main>
+    </>
   );
 }
